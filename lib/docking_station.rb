97f29114b2717634d0d778db826bf8ace6ce65d0
Andrea Wildsmith <awildsmi@hotmail.com>
2016-08-18 12:31:38 +0100

    private methods emply and full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,16 +8,29 @@ attr_reader :bike
   end
 
   def release_bike
-    fail "There are no bikes available" if @bikes.empty?
+    fail "There are no bikes available" if empty?
     @bikes.pop
   end
 
   def dock_bike(bike)
-    fail "This docking station is full" if @bikes.count >= @capacity
+    fail "This docking station is full" if full?
     @bikes << bike
   end
 
+private
+
+def empty?
+  @bikes.empty?
 end
+
+def full?
+  @bikes.count >= 20
+end
+
+end
+
+
+
 
 class Bike
   def working?
